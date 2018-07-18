6c13abdd8760346b6ee7d35f66e3dce75e1b9538
Christina Ng <christinasy317@gmail.com>
2018-07-18 02:03:40 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,7 +2,7 @@
 
 # Try starting out with puts'ing a string.
 
-puts "What is your name?"
+puts "Hi, you've been invited to a party! What is your name?"
 guest_name = gets.chomp.capitalize 
 
 puts "What is the party?"
